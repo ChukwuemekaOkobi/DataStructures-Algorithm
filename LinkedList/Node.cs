37c0f9d806f2d45a585a176ff4cc0c51be2dd807
Chukwuemeka Okobi <emeka006@outlook.com>
2020-06-23 22:30:52 +0100

    Add project files.

---
+++ LinkedList/Node.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList
+{
+    public class Node
+    {
+        //reference to the next node in sequence 
+        public Node Next;
+        public int Value;
+
+        public Node(int data)
+        {
+            Value = data;
+            Next = null;
+        }
+    }
+
+
+}
