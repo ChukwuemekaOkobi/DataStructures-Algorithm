3ca74d4ca5f4be3113a3e5b189289082b995d7cd
chukwuemekaokobi <emekafrancis006@gmail.com>
2022-01-27 20:28:07 +0200

    updated problem solutions

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System;
 using DataStructures.Queues;
 using DataStructures.HashTable;
+using System.Linq;
+using System.Diagnostics;
 
 namespace MainProject
 {
@@ -16,10 +18,15 @@ namespace MainProject
         {
 
 
-            var sentence = "i am a good boy, i hate the cold";
+            var number = new int[]  {1, 7, 7, 5, 9, 2, 12,3, 3};
 
-            Console.WriteLine(FirstNoneRepeatingCharacter(sentence));
-            Console.WriteLine(FirstRepeatingCharacter(sentence));
+
+  
+
+            Console.WriteLine(PairsWithDifference2(number, 2));
+    
+
+
         }
 
         //Stack Problems
@@ -136,6 +143,96 @@ namespace MainProject
             }
 
             return char.MinValue;
+        }
+
+        static int MostFrequentNumber (int[] numbers)
+        {
+            Dictionary<int, int> numberCount = new Dictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (numberCount.ContainsKey(number))
+                {
+                    numberCount[number] += 1;
+                }
+                else
+                {
+                    numberCount.Add(number, 1);
+                }
+            }
+
+            var highestFrequency = 0;
+            var numbe = 0; 
+
+            foreach (var number in numberCount.Keys)
+            {
+                if(numberCount[number] > highestFrequency)
+                {
+                    numbe = number;
+                    highestFrequency = numberCount[number];
+                }
+            }
+
+            return numbe;
+        }
+   
+        //O(n^2) this loops through all values one after the other
+        static int PairsWithDifference (int[] values, int diff)
+        {
+            int count = 0;
+       
+            var numbers = values.Distinct().ToArray();
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                int shift = 1 + i;
+                while(shift < numbers.Length)
+                {
+                    if(Math.Abs(numbers[i]-numbers[shift])  == diff)
+                    {
+                        count++; 
+                    }
+    
+                    shift++;
+                }
+            }
+            return count; 
+        }
+
+        //implementing the using Sliding window O(n)
+        static int PairsWithDifference2(int[] numbers, int diff)
+        {
+            int count = 0;
+
+            System.Array.Sort(numbers);
+
+            int left = 0;
+            int right = 0;
+            while (right < numbers.Length)
+            {
+                if (numbers[right] - numbers[left] == diff)
+                {
+                    count++;
+                    left++;
+                    right++;
+                }
+                else if (numbers[right] - numbers[left] > diff)
+                    left++;
+    
+                else 
+                    right++;
+            }
+            return count;
+
+        }
+
+        
+        static string TwoSums(int[] numbers, int diff)
+        {
+   
+       
+
+            return null;
         }
     }
 }
