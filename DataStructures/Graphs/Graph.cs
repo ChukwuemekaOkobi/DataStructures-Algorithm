a13e22a363dcb8a0d6e16a289a1f225130fbdcd6
chukwuemekaokobi <emekafrancis006@gmail.com>
2022-02-21 20:58:29 +0200

    Add graphs

---
+++ DataStructures/Graphs/Graph.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graphs
+{
+    public class Graph
+    {
+
+        private Dictionary<string, Node> Nodes;
+        public Graph()
+        {
+            Nodes = new Dictionary<string, Node>();
+        }
+
+
+        public void AddNode(string label)
+        {
+            if (!Nodes.ContainsKey(label))
+            {
+                Nodes.Add(label, new Node(label));
+            }
+        }
+    }
+
+
+    public class Node
+    {
+        public string Label { get; private set; }
+
+        public Node(string label)
+        {
+            Label = label;
+        }
+
+    }
+}
